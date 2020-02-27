58324afc4c1dd39c3ddf7f05b540c46f3cfffe15
byun kyoung won <bkwon72@gmail.com>
2020-02-27 14:19:56 +0900

    port 수정 8080 -> 8888

--- src/main/java/pe/bkwon/springcloud/configserver/ConfigServerApplication.java
+++ src/main/java/pe/bkwon/springcloud/configserver/ConfigServerApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+/**
+ * http://10.160.1.236:8888/configsvc/dev
+ */
 @SpringBootApplication
 @EnableConfigServer
 public class ConfigServerApplication {
@@ -11,5 +14,4 @@ public class ConfigServerApplication {
     public static void main(String[] args) {
         SpringApplication.run(ConfigServerApplication.class, args);
     }
-
 }
